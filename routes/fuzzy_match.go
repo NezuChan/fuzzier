42ea3fd45ba8ef292e7b70d4c3a574a5e8cd7dbe
kagchi <yonuxbot@gmail.com>
2023-05-13 19:04:12 +0700

    fix: filter for duplicates

--- routes/fuzzy_match.go
+++ routes/fuzzy_match.go
@@ -21,100 +21,107 @@ type FindMatch struct {
 }
 
 func FuzzyMatch(c *fiber.Ctx) error {
-	authHeader := c.Get("Authorization")
-	if authHeader != config.Conf.Authorization {
-		return c.Status(403).JSON(_struct.BaseJSONResponse{
-			Message: "You are not authorized to to that !",
-			Status:  403,
-		})
-	}
+    authHeader := c.Get("Authorization")
+    if authHeader != config.Conf.Authorization {
+        return c.Status(403).JSON(_struct.BaseJSONResponse{
+            Message: "You are not authorized to to that !",
+            Status:  403,
+        })
+    }
 
-	var body FindMatch
-	err := c.BodyParser(&body)
-	if err != nil && c.Query("query") == "" {
-		log.Infof("Failed to parse body from client")
-		return c.Status(400).JSON(_struct.BaseJSONResponse{
-			Message: "Failed to parse body you sent",
-			Status:  400,
-		})
-	}
+    var body FindMatch
+    err := c.BodyParser(&body)
+    if err != nil && c.Query("query") == "" {
+        log.Infof("Failed to parse body from client")
+        return c.Status(400).JSON(_struct.BaseJSONResponse{
+            Message: "Failed to parse body you sent",
+            Status:  400,
+        })
+    }
 
-	values, _ := redis.Client.LRange(context.Background(), config.Conf.RedisKey, 0, -1).Result()
-	results := make([]string, len(values))
+    values, _ := redis.Client.LRange(context.Background(), config.Conf.RedisKey, 0, -1).Result()
+    resultsMap := make(map[string]bool) // Use a map to keep track of unique strings
 
-	for i, v := range values {
-		var p FindMatch
-		err := json.Unmarshal([]byte(v), &p)
-		if err != nil {
-			panic(err)
-		}
+    for _, v := range values {
+        var p FindMatch
+        err := json.Unmarshal([]byte(v), &p)
+        if err != nil {
+            panic(err)
+        }
 
-		// Convert all strings to lowercase
-		title := strings.ToLower(p.Title)
-		artist := strings.ToLower(p.Artist)
-		duration := p.Duration
+        // Convert all strings to lowercase
+        title := strings.ToLower(p.Title)
+        artist := strings.ToLower(p.Artist)
+        duration := p.Duration
 
-		results[i] = fmt.Sprintf("%s - %s - %d", title, artist, duration)
-	}
+        s := fmt.Sprintf("%s - %s - %d", title, artist, duration)
+        resultsMap[s] = true // Add the string to the map
+    }
 
-	// Convert the search strings to lowercase
-	title := strings.ToLower(body.Title)
-	artist := strings.ToLower(body.Artist)
-	duration := body.Duration
+    // Convert the map keys back to a slice
+    results := make([]string, 0, len(resultsMap))
+    for s := range resultsMap {
+        results = append(results, s)
+    }
 
-	query := c.Query("query", fmt.Sprintf("%s - %s - %d", title, artist, duration))
+    // Convert the search strings to lowercase
+    title := strings.ToLower(body.Title)
+    artist := strings.ToLower(body.Artist)
+    duration := body.Duration
 
-	// Perform the search
-	fuzzyResults := fuzzy.Find(query, results)
+    query := c.Query("query", fmt.Sprintf("%s - %s - %d", title, artist, duration))
 
-	var lastResults []FindMatch
+    // Perform the search
+    fuzzyResults := fuzzy.Find(query, results)
 
-	for _, value := range fuzzyResults {
-		parts := strings.Split(value, " - ")
-		if len(parts) != 3 {
-			continue
-		}
+    var lastResults []FindMatch
 
-		// Convert duration to int
-		duration := 0
-		_, err := fmt.Sscanf(parts[2], "%d", &duration)
-		if err != nil {
-			continue
-		}
+    for _, value := range fuzzyResults {
+        parts := strings.Split(value, " - ")
+        if len(parts) != 3 {
+            continue
+        }
 
-		// Find the original case of the title and artist
-		originalTitle, originalArtist := "", ""
-		for _, v := range values {
-			var p FindMatch
-			err := json.Unmarshal([]byte(v), &p)
-			if err != nil {
-				continue
-			}
-			if strings.ToLower(p.Title) == parts[0] {
-				originalTitle = p.Title
-			}
-			if strings.ToLower(p.Artist) == parts[1] {
-				originalArtist = p.Artist
-			}
-			if originalTitle != "" && originalArtist != "" {
-				break
-			}
-		}
+        // Convert duration to int
+        duration := 0
+        _, err := fmt.Sscanf(parts[2], "%d", &duration)
+        if err != nil {
+            continue
+        }
 
-		// Create a new FindMatch object with the original case of the title and artist
-		s := FindMatch{
-			Title:    originalTitle,
-			Artist:   originalArtist,
-			Duration: duration,
-		}
+        // Find the original case of the title and artist
+        originalTitle, originalArtist := "", ""
+        for _, v := range values {
+            var p FindMatch
+            err := json.Unmarshal([]byte(v), &p)
+            if err != nil {
+                continue
+            }
+            if strings.ToLower(p.Title) == parts[0] {
+                originalTitle = p.Title
+            }
+            if strings.ToLower(p.Artist) == parts[1] {
+                originalArtist = p.Artist
+            }
+            if originalTitle != "" && originalArtist != "" {
+                break
+            }
+        }
 
-		lastResults = append(lastResults, s)
-	}
+        // Create a new FindMatch object with the original case of the title and artist
+        s := FindMatch{
+            Title:    originalTitle,
+            Artist:   originalArtist,
+            Duration: duration,
+        }
 
-	if len(lastResults) > 0 {
-		return c.JSON(lastResults)
-	}
+        lastResults = append(lastResults, s)
+    }
 
-	// Return an empty array if no results were found
-	return c.JSON([]FindMatch{})
+    if len(lastResults) > 0 {
+        return c.JSON(lastResults)
+    }
+
+    // Return an empty array if no results were found
+    return c.JSON([]FindMatch{})
 }
